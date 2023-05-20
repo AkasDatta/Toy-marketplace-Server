06ad16c31ffbae289f1ce50994efc944ec70e8e9
Akas_Datta <012akasdatta@gmail.com>
2023-05-20 17:54:59 +0600

    added addtoys data from mongodb

--- index.js
+++ index.js
@@ -26,12 +26,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const categoryCollection = client.db('toyMarketPlace').collection('animalCategory');
+    const addToyCollection = client.db('toyMarketPlace').collection('addtoys');
+
+    app.get('/category', async(req, res) => {
+        const cursor = categoryCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    });
+
+    // addToys
+    app.post('/addtoys', async(req, res) => {
+        const addtoys = req.body;
+        console.log(addtoys);
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
